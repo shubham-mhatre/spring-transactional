785e8c68c176aa416bd626647464147408216a4d
shubham-mhatre <mhatreshubham03@gmail.com>
2025-01-19 18:02:32 +0530

    project setup initial commit

---
+++ src/test/java/com/sm/tnx/SpringTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.tnx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
