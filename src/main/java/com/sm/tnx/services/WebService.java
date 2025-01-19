50dfdcfe0f96968e47b22d4b3beb8c1f9289ac2e
shubham-mhatre <mhatreshubham03@gmail.com>
2025-01-19 21:43:39 +0530

    refactor webservice

--- src/main/java/com/sm/tnx/services/WebService.java
+++ src/main/java/com/sm/tnx/services/WebService.java
@@ -1,6 +1,8 @@
 package com.sm.tnx.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sm.tnx.dto.EmployeDto;
 import com.sm.tnx.entity.Address;
@@ -17,36 +19,25 @@ public class WebService {
 	private final EmployeeRepository employeeRepository;
 	private final AddressRepository addressRepository;
 
-	public Employee saveEmployee(EmployeDto employeeDto) {
-		
-		Employee employee=saveEmployeeToDb(employeeDto);
-		
-		saveAddressToDb(employeeDto);
-		
-		return employee;
-		
+	//No transactional implemeted
+	public Employee saveEmployee(EmployeDto employeeDto) {		
+		Employee employee=saveEmployeeToDb(employeeDto);//save data in employee table
+		saveAddressToDb(employeeDto);//save data in address table		
+		return employee;		
 	}
-
-	private void saveAddressToDb(EmployeDto employeeDto) {
-		Address address =Address.builder().addr1(employeeDto.getAddr1())
-				.addr2(employeeDto.getAddr2())
-				.city(employeeDto.getCity())
-				.state(employeeDto.getState())
-				.build();
-		
-		addressRepository.save(address);
-		
-	}
-
+	
+	//save data in employee table
 	private Employee saveEmployeeToDb(EmployeDto employeeDto) {
-		Employee employee = Employee.builder().email(employeeDto.getEmail())
-				.gender(employeeDto.getGender())
-				.name(employeeDto.getName())
-				.number(employeeDto.getNumber())
-				.build();
-		
-		return employeeRepository.save(employee);
-		
+		Employee employee = Employee.builder().email(employeeDto.getEmail()).gender(employeeDto.getGender())
+				.name(employeeDto.getName()).number(employeeDto.getNumber()).build();		
+		return employeeRepository.save(employee);		
+	}
+	
+	//save data in address table
+	private void saveAddressToDb(EmployeDto employeeDto) {
+		Address address =Address.builder().addr1(employeeDto.getAddr1()).addr2(employeeDto.getAddr2())
+				.city(employeeDto.getCity()).state(employeeDto.getState()).build();		
+		addressRepository.save(address);		
 	}
 
 }
